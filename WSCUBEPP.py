68c0ae6c9b45a56ad8ad4a418ae886f37e320284
Rahul Kumar <rahul790303@gmail.com>
2024-10-03 11:30:00 +0530

    Update WSCUBEPP.py

--- WSCUBEPP.py
+++ WSCUBEPP.py
@@ -5,4 +5,8 @@
 #wap to take details from a student for ID-Card and then print it in different Lines.
 #wap to take an user i/p as integer then convert it into Float.
 #second set
-
+#wap to check if a number is Positive.
+#wap to check whether a number is odd or even.
+#wap to create area calculator.
+#wap to check whether the passed letter is vowel or not.
+#wap to check if a number is a single digit number,2 digit number & so on upto 5 digits.
