7c1bc4e34c2561391d176a0e6aa81c8bbe338f41
Rahul Kumar <rahul790303@gmail.com>
2024-10-03 11:29:31 +0530

    Update WSCUBEPP.py

--- WSCUBEPP.py
+++ WSCUBEPP.py
@@ -1,1 +1,6 @@
 #First Set
+#Wap to display a Person name, age and address in 3 different lines.
+#wap to swap two variables.
+#wap to convert a float into integer.
+#wap to take details from a student for ID-Card and then print it in different Lines.
+#wap to take an user i/p as integer then convert it into Float.
