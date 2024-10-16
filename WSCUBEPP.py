dd35de3ea23fac8bb4f398fd6fc5325c25f2f215
Rahul Kumar <rahul790303@gmail.com>
2024-10-16 19:24:28 +0530

    Update WSCUBEPP.py

--- WSCUBEPP.py
+++ WSCUBEPP.py
@@ -251,6 +251,12 @@ for i in range(pattern,0,-1):
 **
 *
 """
+pattern = int(input("Enter the Number : "))
+for i in range(pattern,0,-1):
+    for j in range(i):
+        print("*",end="")
+
+    print()
 """
    *
   **
