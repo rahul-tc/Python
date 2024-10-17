444fc03803919434a16b97a969704b001677b05a
Rahul Kumar <rahul790303@gmail.com>
2024-10-17 14:44:25 +0530

    Update WSCUBEPP.py

--- WSCUBEPP.py
+++ WSCUBEPP.py
@@ -292,6 +292,15 @@ for i in range(1, pattern + 1):
 **
 *
 """
+rows = int(input("Enter the Number of rows : "))
+for i in range(1,rows+1):
+    for j in range(1,i+1):
+        print("*",end="")
+    print()
+for k in range(rows-1,0,-1):
+    for l in range(1,k+1):
+        print("*",end="")
+    print("")
 """
 1
 2 4
