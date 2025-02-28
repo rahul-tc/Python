e460d0760b4347a0eaef89193b7ed213439ed596
Rahul Kumar <rahul790303@gmail.com>
2025-02-28 20:14:04 +0530

    Update Recode.py

--- Recode.py
+++ Recode.py
@@ -1,2 +1,8 @@
-
+#Wap to display a person name age address
+Name = input("Enter the name : ")
+Age = int(input("Enter the age: "))
+Address = input("Enter the address: ")
+print(Name)
+print(Age)
+print(Address)
 
