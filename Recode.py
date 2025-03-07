9ade190997feeaedd88fe83065990cbe92fc012f
Rahul Kumar <rahul790303@gmail.com>
2025-03-07 16:13:30 +0530

    Update Recode.py

--- Recode.py
+++ Recode.py
@@ -8,26 +8,8 @@ print(Address)
 
 ########################################################
 
-ABT = CALCULATE(AVERAGE(RidersTable[Confirmation Time]),RidersTable[Confirmed]="Yes")
-ANCT = CALCULATE(AVERAGE(RidersTable[Confirmation Time]),RidersTable[Confirmed]="no")
-Avg Confirmation Time = AVERAGE(RidersTable[Confirmation Time])
-Confirmed Ride% = DIVIDE(CALCULATE(COUNTROWS(RidersTable),RidersTable[Confirmed]="Yes"),[Total Rides])
-Confirmed Rides = CALCULATE(COUNTROWS(RidersTable),RidersTable[Confirmed]="Yes")
-Overall ABT = CALCULATE(AVERAGE(RidersTable[Confirmation Time]),ALL(RidersTable[ZScore Category]), RidersTable[Confirmed]="Yes")
-Overall ANCT = CALCULATE(AVERAGE(RidersTable[Confirmation Time]),ALL(RidersTable[ZScore Category]),ABS( RidersTable[Confirmed]="no"))
-Total Rides = DISTINCTCOUNT(RidersTable[Ride ID])
-UnConfirmed Ride% = DIVIDE(CALCULATE(COUNTROWS(RidersTable),RidersTable[Confirmed]="No"),[Total Rides])
-Unconfirmed Rides = CALCULATE(COUNTROWS(RidersTable),RidersTable[Confirmed]="No")
-
-
-ZScore = (RidersTable[Confirmation Time]-RELATED(OriginAreaTable[Average Confirmation Time]))/RELATED(OriginAreaTable[SD])
-ZScorebins
-ZScore Category = IF(RidersTable[ZScore]>2,"Outliers","Normal")
-
-
-#########################AR-15############################
-collab = https://colab.research.google.com/drive/1AHo8JK17NfGHeEyjqLlwxkqK2FKAz99z?usp=sharing
-
-Correct - cumulative_reason_percentage = round(sorted_reasons1.cumsum()/sorted_reasons1.sum()*100,2)
-cumulative_reason_percentage
-#made the above changes and then  play will be able to solve the error.
+import pandas as pd
+import matplotlib.pyplot as plt
+product_return = pd.read_csv("https://raw.githubusercontent.com/Invact-Abhay/DOE/refs/heads/main/ComplaintsTable.csv")
+product_return
+product_return.head()
