0aefae31ef24d272486efeacca9aee89d2aa421e
Rahul Kumar <rahul790303@gmail.com>
2025-08-01 20:41:11 +0530

    Update Recode.py

--- Recode.py
+++ Recode.py
@@ -17,3 +17,11 @@ print(Address)
 #Wap to convert a float into integer.
 Num = float(input("Enter a Number : "))
 print(int(Num))
+
+#wap to take details from a student for ID-Card and then print it in different Lines.
+Name = input("Enter the name: ")
+Roll = input("Enter the Roll: ")
+Batch = input("Enter the Batch: ")
+print(Name)
+print(Roll)
+print(Batch)
