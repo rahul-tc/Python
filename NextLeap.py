2e6c132f453727d8f5274b23b4c0a72e3e6cf08d
Rahul Kumar <rahul790303@gmail.com>
2025-08-29 13:49:14 +0530

    Update NextLeap.py

--- NextLeap.py
+++ NextLeap.py
@@ -3,3 +3,9 @@ class Solution:
     def helloWorld(self):
         # Your code goes here
         print("Hello World")
+
+#2 - Print Two Lines
+class Solution:
+	def printTwoLines(self):
+		print("Hello there!")
+		print("Let's start")
