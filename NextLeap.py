974bc5ec90a46d7f849d65258bfd88a19866d810
Rahul Kumar <rahul790303@gmail.com>
2025-09-04 12:11:56 +0530

    Update NextLeap.py

--- NextLeap.py
+++ NextLeap.py
@@ -22,5 +22,9 @@ class Solution:
         return str(num)
 
 #5 - Add Two Numbers
+class Solution:
+    def addTwoNumbers(self, a: int, b: int) -> int:
+        # Your code goes here
+        return a + b
 
 
