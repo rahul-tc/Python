44efbe974b1a46276dff7afb7f5017b0eaf6cdf1
Rahul Kumar <rahul790303@gmail.com>
2026-04-08 16:50:17 +0530

    Update Recode.py

--- Recode.py
+++ Recode.py
@@ -6,6 +6,7 @@ print(Name)
 print(Age)
 print(Address)
 
+
 #Wap to display a Person name, age and address in 3 different lines.
 Name = input("Enter the name: ")
 Age = int(input("Enter the age: "))
